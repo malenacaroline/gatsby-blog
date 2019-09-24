38b79e719006e4b7442b5b7b3993c48c4f261c75
malenacaroline <malena@ciclic.com.br>
2019-09-24 17:35:20 -0300

    organizing files and adding links

--- my-blog/src/pages/index.js
+++ my-blog/src/pages/index.js
@@ -1,20 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Home</h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{color: 'red'}}>Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About (Gatsby Link)</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
