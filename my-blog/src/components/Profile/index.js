1abbc24ba979f72bf06c975b2856ea3707754c12
malenacaroline <malena@ciclic.com.br>
2019-09-24 17:56:25 -0300

    removing links and creating profile component

---
+++ my-blog/src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Matt Foo</h1>
+        <h2>Programador Backend</h2>
+        <p>
+            Membro da Empresa Foo, escrevo sobre backend e outras coisas mais.
+        </p>
+    </div>
+)
+
+export default Profile
